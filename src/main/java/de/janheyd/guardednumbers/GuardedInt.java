49f16e4f0471eda5bf2610b1add37fca9537cf59
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-12-01 23:22:42 +0100

    added delegate methods for *Exact methods from java.lang.Math to class GuardedInt

--- src/main/java/de/janheyd/guardednumbers/GuardedInt.java
+++ src/main/java/de/janheyd/guardednumbers/GuardedInt.java
@@ -2,6 +2,8 @@ package de.janheyd.guardednumbers;
 
 import java.io.Serializable;
 
+import static java.lang.Math.*;
+
 public final class GuardedInt implements Serializable {
 
 	private final int value;
@@ -12,6 +14,34 @@ public final class GuardedInt implements Serializable {
 
 	public int toInt() {
 		return value;
+	}
+
+	public GuardedInt plus(GuardedInt number) {
+		return plus(number.toInt());
+	}
+
+	public GuardedInt plus(int number) {
+		return new GuardedInt(addExact(value, number));
+	}
+
+	public GuardedInt minus(GuardedInt number) {
+		return minus(number.toInt());
+	}
+
+	public GuardedInt minus(int number) {
+		return new GuardedInt(subtractExact(value, number));
+	}
+
+	public GuardedInt times(GuardedInt number) {
+		return times(number.toInt());
+	}
+
+	public GuardedInt times(int number) {
+		return new GuardedInt(multiplyExact(value, number));
+	}
+
+	public GuardedInt negate() {
+		return new GuardedInt(negateExact(value));
 	}
 
 }
